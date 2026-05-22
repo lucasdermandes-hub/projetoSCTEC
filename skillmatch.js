d4bbe63fac4092dfff411a69960400b4b0399625
lucasdermandes-hub <lucasdermandes@gmail.com>
2026-05-22 11:16:41 -0300

    primeira commit

---
+++ skillmatch.js
@@ -0,0 +1,39 @@
+// RF01 - Criar o perfil do candidato
+const candidato = {
+  nome: "João Silva",
+  area: "Front-End",
+  habilidades: ["JavaScript", "CSS", "Lógica de Programação", "HTML", "GitHub"],
+  experienciaMeses: 7
+};
+
+// REF02 - Criar uma lista de vagas (mínimo 3)
+const vagas = [
+    {
+    id: 1,
+    empresa: "TechStart",
+    cargo: "Desenvolvedor Front-End Júnior",
+    requisitos: ["JavaScript", "GitHub", "Lógica da Programação", "Kanbam"],
+    salario: 3000,
+    modalidade: "Home Office"
+    },
+    
+    {
+    id: 2,
+    empresa: "CodeLab",
+    cargo: "Estágio Front-End",
+    requisitos: ["JavaScript", "Kanban", "GitHub", "HTML", "CSS"],
+    salario: 1600,
+    modalidade: "Híbrido"
+  },
+  {
+    id: 3,
+    empresa: "WebSolutions",
+    cargo: "Programador JavaScript Júnior",
+    requisitos: ["JavaScript", "Arrays", "Objetos", "Funções"],
+    salario: 3400,
+    modalidade: "Presencial"
+  }
+  
+];
+
+
