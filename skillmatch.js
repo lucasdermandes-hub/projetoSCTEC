bc435c74c7385658976393267ade71546009a5a9
lucasdermandes-hub <lucasdermandes@gmail.com>
2026-05-23 22:33:47 -0300

    segundo commit

--- skillmatch.js
+++ skillmatch.js
@@ -36,4 +36,36 @@ const vagas = [
   
 ];
 
+// RF09 e RF11 - Criar uma classe base e demonstrar uso do this
+class Vaga {
+  constructor(empresa, cargo, requisitos, salario, modalidade) {
+    this.empresa = empresa;
+    this.cargo = cargo;
+    this.requisitos = requisitos;
+    this.salario = salario;
+    this.modalidade = modalidade;
+  }
 
+  // Método que usa o 'this'
+  exibirResumo() {
+    return `${this.cargo} na empresa ${this.empresa}`;
+  }
+}
+
+// RF10 - Usar herança (VagaFrontEnd herda de Vaga)
+class VagaFrontEnd extends Vaga {
+  constructor(empresa, cargo, requisitos, salario, modalidade, nivel) {
+    // O super chama o construtor da classe pai (Vaga)
+    super(empresa, cargo, requisitos, salario, modalidade);
+    this.nivel = nivel; // Atributo exclusivo da vaga front-end
+  }
+
+  exibirNivel() {
+    return `Nível da vaga: ${this.nivel}`;
+  }
+}
+
+// RF13- Usar closure (vai manter um contador para análisar quantas análises foram feitas)
+function criarContadorAnalises() {
+  
+}
